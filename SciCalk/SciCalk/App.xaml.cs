5ea414881c4440c6822090910af01c1058627949
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-11-12 12:42:13 +0200

    12.11.25
    Windows

--- SciCalk/SciCalk/App.xaml.cs
+++ SciCalk/SciCalk/App.xaml.cs
@@ -1,15 +1,41 @@
-﻿namespace SciCalk
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+using SciCalk.Views;
+
+namespace SciCalk
 {
     public partial class App : Application
     {
+        const int WindowWidth = 1080;
+        const int WindowHeight = 1920;
+
         public App()
         {
             InitializeComponent();
+
+#if WINDOWS
+    Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+    {
+        var mauiWindow = handler.VirtualView;
+        var nativeWindow = handler.PlatformView;
+        nativeWindow.Activate();
+        IntPtr windowHandler = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+        WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandler);
+        AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+    });
+#endif
+
+            MainPage = new CalculatorPage();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //}
+        //    return new Window(new CalculatorPage());
+        //}
     }
 }
