753e3769cc2c86abeb177501cde090d20d839778
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-11-18 15:47:59 +0200

    18.11.25
    Mnnm

--- SciCalk/SciCalk/ViewModels/CalculatorPageViewModel.cs
+++ SciCalk/SciCalk/ViewModels/CalculatorPageViewModel.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 
 namespace SciCalk.ViewModels
 {
-    class CalculatorPageViewModel
+    [INotifyPropertyChanged]
+    internal partial class CalculatorPageViewModel
     {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
     }
 }
