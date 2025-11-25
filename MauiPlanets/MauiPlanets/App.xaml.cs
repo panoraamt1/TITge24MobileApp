580236f088b3d2b1904b2408b3fc5cdf05496d08
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-11-25 15:49:00 +0200

    25.11.25
    Screen Size

--- MauiPlanets/MauiPlanets/App.xaml.cs
+++ MauiPlanets/MauiPlanets/App.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace MauiPlanets
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+namespace MauiPlanets
 {
     public partial class App : Application
     {
+
+        const int WindowWidth = 1080;
+        const int WindowHeight = 1920;
         public App()
         {
             InitializeComponent();
+
+#if WINDOWS
+    Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+    {
+        var mauiWindow = handler.VirtualView;
+        var nativeWindow = handler.PlatformView;
+        nativeWindow.Activate();
+        IntPtr windowHandler = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+        WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandler);
+        AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+    });
+#endif
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
