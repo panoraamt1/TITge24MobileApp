2e859dad134b1835babac87521a514afd56aca73
panoraamt1 <106094230+panoraamt1@users.noreply.github.com>
2025-12-03 11:02:08 +0200

    03.12.25
    MauiCRUD

---
+++ MauiCRUD/MauiCRUD/Data/DatabaseContext.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq.Expressions;
+using SQLite;
+
+namespace MauiCRUD.Data
+{
+    public class DatabaseContext
+    {
+        private const string DbName = "CRUDdb1" + "";
+        private static string DbPath => Path.Combine(".", DbName);
+        private SQLiteAsyncConnection _connectsion;
+
+        private SQLiteAsyncConnection Database =>
+            (_connectsion ??= new SQLiteAsyncConnection(DbPath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
+
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
+        }
+
+        public async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable: class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return Database.Table<TTable>();
+        }
+
+        public async Task CreateTableIfNotExists<TTable>() where TTable: class, new()
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        private async Task<TResult> Execute<TTable, TResult>(Func<Task<TResult>> action) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await action();
+        }
+
+        public async Task<TTable> GeyItemByKeyAsync<TTable>(object primaryKey) where TTable : class, new()
+        {
+            return await Execute<TTable, TTable>(async () => await Database.GetAsync<TTable>(primaryKey));
+        }
+
+        public async Task<bool> AddItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            return await Execute<TTable, bool>(async () => await Database.InsertAsync(item) > 0);
+        }
+
+        public async Task<bool> UpdateItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.UpdateAsync(item) > 0;
+        }
+
+        public async Task<bool> DeleateItemAsync<TTable>(object primaryKey) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.UpdateAsync(primaryKey) > 0;
+        }
+
+        public async ValueTask DisposeAsync() => await _connectsion.CloseAsync();
+        public async Task<IEnumerable<TTable>> GetFilteredAsync<TTable>(Expression<Func<TTable, bool>> predicate) where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.Where(predicate).ToListAsync();
+        }
+    }
+}
---
+++ MauiCRUD/MauiCRUD/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiCRUD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product Clone() => MemberwiseClone() as Product;
+        public (bool IsValid, string? ErrorMessage) Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return (false, $"{nameof(Name)} is required");
+            }
+            else if (Price <= 0)
+            {
+                return (false, $"{nameof(Price)} should be grater than 0");
+            }
+            return (true, null );
+        }
+    }
+}
